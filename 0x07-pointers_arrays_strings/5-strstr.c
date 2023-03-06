6e9aed48799ea7f0204de2d0b54f59060df81397
AnassMajidi13 <younextvictim@gmail.com>
2023-03-06 14:00:22 -0800

    correction

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
 
-  /**
-   * _strstr - locate a substring
-   * @haystack: string
-   * @needle: string substring
-   * Return: Pointer to beginning of substring, or NULL if not found.
-   */
-  
-  char *_strstr(char *haystack, char *needle)
-  {
-    int i, j, c;
-  
-    i = 0;
-    c = 0;
-    while (haystack[i] != '\0')
-    {
-      j = 0;
-      while (needle[j + c] != '\0' && haystack[i + c] != '\0'
-             && needle[j + c] == haystack[i + c])
-      {
-        if (haystack[i + c] != needle[j + c])
-          break;
-        c++;
-      }
-      if (needle[j + c] == '\0')
-        return (&haystack[i]);
-      j++;
-      i++;
-    }
-  
-    return (NULL);
-  }
+/**
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ **/
+
+char  *_strstr(char *haystack, char *needle)
+{
+	char str1, *str2; /*Declaring variables/
+
+	while (*haystack != '\0')
+	{
+		str1 = haystack; /values/
+		str2 = needle;
+
+		/Star WHILE/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+		{
+			haystack++;
+			str2++;
+		}
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
+	}
+	return (0);
+}
